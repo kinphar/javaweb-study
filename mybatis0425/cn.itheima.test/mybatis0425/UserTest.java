c3a9b1cadf03f732c799c8959cb6be6e6f14073f
dingqingfa <dingqingfa@star-net.cn>
2017-04-25 19:10:06 +0800

    mybatis 数据库insert。

--- mybatis0425/cn.itheima.test/mybatis0425/UserTest.java
+++ mybatis0425/cn.itheima.test/mybatis0425/UserTest.java
@@ -1,6 +1,8 @@
 package mybatis0425;
 
 import java.io.InputStream;
+import java.util.Date;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -22,5 +24,38 @@ public class UserTest {
 		User user = openSession.selectOne("test.findUserById", 1);
 		System.out.println(user);
 		openSession.close();
+	}
+	
+	@Test
+	public void testFindUserByUserName() throws Exception{
+		String resource = "SqlMapConfig.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
+		SqlSession openSession = factory.openSession();
+		
+		List<User> List = openSession.selectList("test.findUserByUserName", "Õı");		
+		System.out.println(List);
+		
+	}
+	
+	@Test
+	public void testInsertUser() throws Exception{
+		String resource = "SqlMapConfig.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
+		SqlSession openSession = factory.openSession();
+		
+		User user = new User();
+		user.setUsername("’‘Œ‰");
+		user.setBirthday(new Date());
+		user.setSex("1");
+		user.setAddress("∏£Ω®∏£÷›");
+		
+		System.out.println("===" + user.getId());
+		
+		openSession.insert("test.insertUser", user);
+		openSession.commit();
+		
+		System.out.println("===" + user.getId());
 	}
 }
