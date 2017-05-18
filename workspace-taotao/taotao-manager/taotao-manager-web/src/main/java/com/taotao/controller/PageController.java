bf2a5dd883d3e91af8ab2a4e32f1dd6cc09a9ef3
dingqingfa <dingqingfa@star-net.cn>
2017-05-18 10:01:01 +0800

    打开其他页面。

--- workspace-taotao/taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/PageController.java
+++ workspace-taotao/taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/PageController.java
@@ -1,6 +1,7 @@
 package com.taotao.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -20,5 +21,13 @@ public class PageController {
 		return "index";
 	}
 	
-	
+	/**
+	 * 展示其他页面。
+	 * @param page
+	 * @return
+	 */
+	@RequestMapping("/{page}")
+	public String showPage(@PathVariable String page) {
+		return page;
+	}
 }
