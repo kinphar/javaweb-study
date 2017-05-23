9a8b1095e251d0301be88675592893e192b874b3
dingqingfa <dingqingfa@star-net.cn>
2017-05-23 18:50:33 +0800

    增加ftpUtil 类与测试。

--- workspace-taotao/taotao-manager/taotao-manager-web/src/test/java/com/taotao/controller/FTPTest.java
+++ workspace-taotao/taotao-manager/taotao-manager-web/src/test/java/com/taotao/controller/FTPTest.java
@@ -7,6 +7,8 @@ import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 import org.junit.Test;
 
+import com.taotao.common.utils.FtpUtil;
+
 public class FTPTest {
 
 	@Test
@@ -21,5 +23,14 @@ public class FTPTest {
 		ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
 		ftpClient.storeFile("dianlu123.jpg", inputStream);
 		ftpClient.logout();
+	}
+	
+	@Test
+	public void testFtpUtils() throws Exception {
+		FileInputStream inputStream = new FileInputStream(
+				new File("D:\\dianlu.jpg"));
+		FtpUtil.uploadFile("192.168.249.132", 21, "ftpuser", "115513875", 
+				"/home/ftpuser/www/images", "/2017/05/23", 
+				"hello.jpg", inputStream);
 	}
 }
