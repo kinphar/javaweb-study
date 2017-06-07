309fb66534274671a1763803dae8ce76b969e8aa
dingqingfa <dingqingfa@star-net.cn>
2017-06-07 16:18:00 +0800

    使用 httpclient 实现 post 测试请求。

--- workspace-taotao/taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
+++ workspace-taotao/taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
@@ -2,6 +2,8 @@ package com.taotao.portal.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -9,5 +11,11 @@ public class IndexController {
 	@RequestMapping("/index")
 	public String showIndex() {
 		return "index";
+	}
+	
+	@RequestMapping(value="/httpclient/post", method=RequestMethod.POST)
+	@ResponseBody
+	public String testPost(String username, String password) {
+		return "username:" + username + "\tpassword:" + password;
 	}
 }
--- workspace-taotao/taotao-portal/src/test/java/com/taotao/httpclient/HttpClientTest.java
+++ workspace-taotao/taotao-portal/src/test/java/com/taotao/httpclient/HttpClientTest.java
@@ -1,11 +1,19 @@
 package com.taotao.httpclient;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 
@@ -47,5 +55,36 @@ public class HttpClientTest {
 		response.close();
 		httpClient.close();
 	}
-
+	
+	@Test
+	public void doPost() throws Exception {
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		HttpPost post = new HttpPost("http://localhost:8082/httpclient/post.html");
+		
+		CloseableHttpResponse response = httpClient.execute(post);
+		String string = EntityUtils.toString(response.getEntity());
+		System.out.println(string);
+		
+		response.close();
+		httpClient.close();
+	}
+	
+	@Test
+	public void doPostWithParam() throws Exception {
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		HttpPost post = new HttpPost("http://localhost:8082/httpclient/post.html");
+		
+		List<NameValuePair> kvList = new ArrayList<>();
+		kvList.add(new BasicNameValuePair("username", "zhang san"));
+		kvList.add(new BasicNameValuePair("password", "123"));
+		StringEntity entity = new UrlEncodedFormEntity(kvList);
+		post.setEntity(entity);
+		
+		CloseableHttpResponse response = httpClient.execute(post);
+		String string = EntityUtils.toString(response.getEntity());
+		System.out.println(string);
+		
+		response.close();
+		httpClient.close();
+	}
 }
