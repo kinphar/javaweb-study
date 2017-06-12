369dd86623c3ce762c07115c8f72fb46fdabcae9
dingqingfa <dingqingfa@star-net.cn>
2017-06-12 10:50:04 +0800

    单机版redis 测试。

--- workspace-taotao/taotao-rest/src/test/java/com/taotao/rest/jedis/JedisTest.java
+++ workspace-taotao/taotao-rest/src/test/java/com/taotao/rest/jedis/JedisTest.java
@@ -3,6 +3,8 @@ package com.taotao.rest.jedis;
 import java.util.HashSet;
 
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.Jedis;
@@ -46,5 +48,16 @@ public class JedisTest {
 		
 		cluster.close();
 	}
-
+	
+	@Test
+	public void testSpringJedisSingle() {
+		ApplicationContext applicationContext = 
+				new ClassPathXmlApplicationContext("classpath:spring/applicationContext-*.xml");
+		JedisPool pool = (JedisPool) applicationContext.getBean("redisClient");
+		Jedis jedis = pool.getResource();
+		String string = jedis.get("key1");
+		System.out.print(string);
+		jedis.close();
+		pool.close();		
+	}
 }
