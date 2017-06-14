0cf6e23f612e8c209ce635ca522353c3ee1429c6
dingqingfa <dingqingfa@star-net.cn>
2017-06-14 16:05:44 +0800

    增加solr query测试用例。

--- workspace-taotao/taotao-rest/src/test/java/com/taotao/rest/solrj/SolrjTest.java
+++ workspace-taotao/taotao-rest/src/test/java/com/taotao/rest/solrj/SolrjTest.java
@@ -1,7 +1,11 @@
 package com.taotao.rest.solrj;
 
+import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
 import org.apache.solr.common.SolrInputDocument;
 import org.junit.Test;
 
@@ -30,5 +34,24 @@ public class SolrjTest {
 		//通过查询删除全部。
 		//solrServer.deleteByQuery("*:*");
 		solrServer.commit();		
+	}
+	
+	@Test
+	public void queryDocument() throws Exception {
+		SolrServer solrServer = new HttpSolrServer("http://192.168.249.132:8080/solr");
+		SolrQuery query = new SolrQuery();
+		query.setQuery("*:*");
+		query.setStart(20);
+		query.setRows(50);
+		QueryResponse response = solrServer.query(query);
+		SolrDocumentList solrDocumentList = response.getResults();
+		System.out.println("total:" + solrDocumentList.getNumFound());
+		for (SolrDocument solrDocument : solrDocumentList) {
+			System.out.println(solrDocument.get("id"));
+			System.out.println(solrDocument.get("item_title"));
+			System.out.println(solrDocument.get("item_price"));
+			System.out.println(solrDocument.get("item_image"));
+			System.out.println("================");
+		}
 	}
 }
