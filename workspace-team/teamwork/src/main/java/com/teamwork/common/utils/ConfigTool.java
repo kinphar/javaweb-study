823ec0fdbfb123a0909bf3602ac40d2be557a666
dingqingfa <dingqingfa@star-net.cn>
2017-08-24 20:18:41 +0800

    知识管理-富文本编辑-初步实现图片可上传，待优化。

---
+++ workspace-team/teamwork/src/main/java/com/teamwork/common/utils/ConfigTool.java
@@ -0,0 +1,23 @@
+package com.teamwork.common.utils;
+
+import org.springframework.beans.factory.InitializingBean;
+
+public class ConfigTool implements InitializingBean{
+	private static String imageSavePath;
+	/**获取图片请求action*/
+	public static final String imageAction = "image";
+
+	public static String getImageSavePath() {
+		return imageSavePath;
+	}
+
+	public void setImageSavePath(String imageSavePath) {
+		ConfigTool.imageSavePath = imageSavePath;
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		//初始化要做的事情
+	}
+	
+}
